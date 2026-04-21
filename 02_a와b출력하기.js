a2503a8d26e7207ef72f18651df7c14168b6e310
sohyunPark <33.sohyun.park@gmail.com>
2026-04-21 21:48:58 +0900

    02,03,04 풀이

---
+++ 02_a와b출력하기.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}).on('line',function(line){
+    const[a,b]=line.split(' ')
+    console.log('a =',a)
+    console.log('b =',b)
+});
+
+// let input = [];
+// rl.on('line',function(line){
+//     input = line.split(' ');
+// }).on('close',function(){
+//     console.log('a = ' + Number(input[0]));
+//     console.log('a = ' + Number(input[1]))
+// })
