29bd389a2e1b5d559832679f2c69e23ca172e330
sohyunPark <33.sohyun.park@gmail.com>
2026-04-23 17:56:51 +0900

    11~15 문자풀이

---
+++ 11_문자열섞기.js
@@ -0,0 +1,12 @@
+function solution(str1, str2) {
+    let str01 = [...str1];
+    let str02 = [...str2];
+    let res = '';
+
+	str01.forEach((ele,index) => {
+        res+=ele+=str02[index]
+    });
+
+    return res
+
+}
